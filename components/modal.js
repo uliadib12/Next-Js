2c4dd25d6cf8c1716c5cb895dcc14e4f1dc98c58
uliadib12 <uliadib@gmail.com>
2022-02-05 16:54:14 +0700

    add: Add hover in dropdownlist

--- components/modal.js
+++ components/modal.js
@@ -1,14 +1,21 @@
 import { useContext } from "react"
 import { modalContex } from "../pages"
+import { GrClose } from "react-icons/gr"
+import { useMediaQuery } from 'react-responsive'
 
 export function Modal(props) {
 
     const modal = useContext(modalContex)
+    const isScreen = useMediaQuery({
+        query: '(max-width: 640px)'
+      })
     
     return (
-        <div className={modal.modalShow ? "" : "hidden"}>
+        <div className={modal.modalShow ? "block" : "hidden"}>
             <div onClick={()=>{modal.setmodalShow(false)}} style={{backgroundColor: 'rgba(0, 0, 0, 0.4)'}} className="fixed flex justify-center items-center inset-0 w-screen h-screen">
-                <div onClick={(event)=>{event.stopPropagation()}} style={{backgroundColor: 'rgba(255, 255, 255, 1)'}} className="bg-stone-900 w-3/4 sm:h-4/5 h-3/5 rounded-xl shadow-lg"/>
+                <div onClick={(event)=>{event.stopPropagation()}} style={{backgroundColor: 'rgba(255, 255, 255, 1)'}} className="relative bg-stone-900 sm:w-3/4 w-11/12 h-4/5 rounded-xl shadow-lg">
+                    <GrClose onClick={()=>{modal.setmodalShow(false)}} size={isScreen ? 30 : 40} className=" absolute right-5 top-5 cursor-pointer"/>
+                </div>
             </div>
         </div>
     )
--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react"
+import { useState} from "react"
 import {IoIosArrowDown} from "react-icons/io"
+import { useRouter } from 'next/router';
 
 export function Navbar(props) {
     return (
-        <nav className='text-white flex justify-center items-center sm:grow sm:justify-center sm:gap-7 gap-9 text-sm sm:text-base'>
+        <nav className='text-white flex justify-center items-center sm:grow sm:justify-center sm:gap-7 gap-2 text-sm sm:text-base'>
           {props.children}    
         </nav>
     )
@@ -27,8 +28,9 @@ export function NavbarList(props) {
 }
 
 export function DropDownList(props) {
+    const router = useRouter();
 
     return (
-        <div className='text-white font-bold px-2 pt-1 pb-1 border-b-2 border-white last:border-b-0'>{props.text}</div>
+        <div onClick={(e)=>{e.stopPropagation(); router.push(`/${props.text}`)}} className='text-white font-bold px-2 pt-1 pb-1 border-b-2 border-white last:border-b-0 cursor-pointer hover:text-red-500'>{props.text}</div>
     )
 }
